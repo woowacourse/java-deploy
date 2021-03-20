d4090bb21d11dfd42628cd2ada916d91976bf314
이동규 <cu@woowahan.com>
2021-03-20 22:49:09 +0900

    feat: json-appender 추가

--- src/main/java/deploy/logback/api/LogBackApiController.java
+++ src/main/java/deploy/logback/api/LogBackApiController.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.time.LocalDateTime;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @RestController
 @RequestMapping
 public class LogBackApiController {
     private static final Logger log = LoggerFactory.getLogger("console");
     private static final Logger fileLogger = LoggerFactory.getLogger("file");
+    private static final Logger json = LoggerFactory.getLogger("json");
 
 
     @GetMapping("/level")
@@ -23,6 +27,7 @@ public class LogBackApiController {
         log.warn("A WARN Message");
         log.error("An ERROR Message");
 
+
         return "로그 레벨 확인 페이지입니다.";
     }
 
@@ -30,6 +35,16 @@ public class LogBackApiController {
     public String logbackTest() {
         log.info("콘솔 로깅 입니다.");
         fileLogger.info("파일 로깅 입니다.");
-        return "로깅 파일을 확인해주세요.";
+        return "file.log 로깅 파일을 확인해주세요.";
+    }
+
+    @GetMapping("/json")
+    public String jsonTest() {
+        json.info("{}, {}, {}",
+            kv("orderNo", 1L),
+            kv("memberNo", 2L),
+            kv("deviceId", 3L)
+        );
+        return "json.log 로깅 파일을 확인해주세요.";
     }
 }
